5060fa629c72827f6c3d21fbfc4972e537a57d29
Sohel-06 <urstruleysohel@gmail.com>
2024-01-29 04:32:16 +0530

    mongo db atlas

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,7 +1,8 @@
 const mongoose=require("mongoose");
+mongoose.set('strictQuery',false)
 const connection=async()=>{
     try{
-  const res= await mongoose.connect('mongodb://127.0.0.1:27017');
+  const res= await mongoose.connect("mongodb+srv://urstruleysohel:fq1uvIzEZFfayKQr@cluster0.k2kvo0r.mongodb.net/Demodb?retryWrites=true&w=majority");
   console.log("Connection is Successfull")
 }
 catch(err){
