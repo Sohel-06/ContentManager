bb266f070e0e72cf79bb87a4ec8efe2aba5e6f1a
Sohel-06 <urstruleysohel@gmail.com>
2024-01-29 02:47:46 +0530

    mongo dynamic connection

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -1,8 +1,8 @@
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/content-manager");
+const val=process.env.MONGODB_CONNECT_URI||"mongodb://localhost:27017/content-manager";
 const connection=async()=>{
     try{
-  const res= await mongoose.connect("mongodb://localhost:27017/content-manager");
+  const res= await mongoose.connect(val);
   console.log("Connection is Successfull")
 }
 catch(err){
